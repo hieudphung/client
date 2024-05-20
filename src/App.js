9fc9dd5079eb1949bc3db03c318ae74f76b4c962
Hieu Phung <hieudphung@gmail.com>
2024-05-19 22:06:00 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import BillSplitter from './components/BillSplitter';
 
-function App() {
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.body.classList.toggle('dark-mode');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${darkMode ? 'dark-mode' : ''}`}>
+      <button className="dark-mode-toggle" onClick={toggleDarkMode}>{darkMode ? '☀️' : '🌙'}</button>
+  
+      <h1> </h1>
+      <div className="container">
+        <div className="bubble">
+          <BillSplitter />
+        </div>
+      </div>
     </div>
   );
 }
